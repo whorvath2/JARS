23da566db09132ed4e7d1261360d67ea35b74374
Bill Horvath <bhorvath@umich.edu>
2025-03-18 20:43:56 -0400

    Adds basic implementation of R scripting call.

--- src/co/deability/lirs/api/blueprints/analytics_blueprint.py
+++ src/co/deability/lirs/api/blueprints/analytics_blueprint.py
@@ -1,7 +1,7 @@
 from http import HTTPStatus
 from typing import Final, Tuple
 
-from flask import Blueprint, jsonify, make_response, request
+from flask import Blueprint, make_response, request
 
 from co.deability.lirs.service import analytics_service
 
@@ -16,5 +16,5 @@ EMPTY_SUCCESS_RESPONSE: Final[Tuple[str, int]] = ("", HTTPStatus.NO_CONTENT)
 @analytics_blueprint.post("/run/rscript")
 def post_script():
     return make_response(
-        jsonify(analytics_service.run(script=request.json)), HTTPStatus.OK
+        str(analytics_service.run(script=request.get_data(as_text=True))), HTTPStatus.OK
     )
--- src/co/deability/lirs/service/analytics_service.py
+++ src/co/deability/lirs/service/analytics_service.py
@@ -1,5 +1,22 @@
-from typing import Dict, Any
+from subprocess import CompletedProcess
 
+import subprocess
 
-def run(script: Dict[str, Any]):
-    return None
+def run(script: str) -> str:
+    """
+    Runs the supplied R script.
+
+    :param script: The script to be run.
+    :return: The output of RScript's execution of the supplied script.
+    """
+    if not validate(script):
+        raise ValueError("You bad scripter you!")
+    completed_process: CompletedProcess = subprocess.run(["Rscript", script], capture_output=True)
+    if completed_process.returncode != 0:
+        raise RuntimeError("You bad Rscript runner you!")
+    return completed_process.stdout.decode("utf-8")
+
+def validate(script: str) -> bool:
+    completed_process: CompletedProcess = subprocess.run(
+        ["Rscript", "-e", f"'parse(text = {script})'"], capture_output=True)
+    return completed_process.returncode == 0
