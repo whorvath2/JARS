46a1a6ec9a1d180e6eb32c6c86cb2f3e89ff68e5
Bill Horvath <bhorvath@umich.edu>
2025-04-16 16:54:19 -0400

    Documentation tweaks.
    Signed-off-by: Bill Horvath <bhorvath@umich.edu>

--- src/co/deability/jars/service/analytics_service.py
+++ src/co/deability/jars/service/analytics_service.py
@@ -2,7 +2,6 @@ from subprocess import CompletedProcess
 import subprocess
 import shlex
 
-from co.deability.jars.config import LOG
 from co.deability.jars.error.r_script_error import RScriptError
 
 
@@ -21,7 +20,6 @@ def run(script: str) -> str:
     completed_process: CompletedProcess = subprocess.run(
         ["Rscript", "-e", r_command], capture_output=True, shell=False
     )
-    LOG.debug(vars(completed_process))
     if completed_process.returncode != 0:
         raise RScriptError(bad_script=script)
     return completed_process.stdout.decode("utf-8")
@@ -29,10 +27,11 @@ def run(script: str) -> str:
 
 def validate(script: str) -> bool:
     """
-    Returns `True` if the supplied R script can be executed by Rscript, `False` otherwise.
+    Returns `True` if the supplied script is valid R code that can be executed by Rscript,
+    `False` otherwise.
 
     :param script: The script to be validated.
-    :return: `True` if the supplied R script can be executed by Rscript, `False` otherwise.
+    :return: `True` if the supplied script can be executed by Rscript, `False` otherwise.
     """
     script = shlex.quote(script)
     r_command = f"str2expression({script});"
